23fa29ed663c6758b2baa4c4f8d6869787993633
Dillon Powers <dillon.c.powers@gmail.com>
2016-09-30 12:03:28 -0400

    basic setup

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 var Express = require('express');
 var app = new Express();
 
-app.get('/', function(req,res,next) {
-	console.log("Ya server is RUNNIN");
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var router = require('./routes');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+// SEQUELIZE IN MODELS FOLDER
+
+
+
+nunjucks.configure('views', {
+	watch: true,
+	express: app,
+	noCache: true
 })
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use(Express.static('public'));
+
+app.use(router);
+
+
+
+app.use(function(req, res, next) {
+	var err = new Error('Page Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	console.error(err);
+	res.render(
+		'/error', {
+			message: err.message
+		}
+	);
+});
 
 app.listen(8080);
